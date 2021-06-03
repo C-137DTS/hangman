c1643981bc832e88b44c20c91f90b42fe8d8c5a9
Juan Pablo <c137dts@gmail.com>
2021-06-02 20:40:44 -0500

    adding level and improving the interface

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 n_words = 171
 random_words = []
 random_lines = [random.randint(1, n_words) for i in range(1, 4)]
+levels = 3
 
 
 
@@ -18,10 +19,13 @@ def run():
             line += 1
             if line == random_lines[0] or line == random_lines[1] or line == random_lines[2]:
                 random_words.append(word.strip())
-    print(random_words[0])
-    game(random_words[0])
+    for i in range(0, levels):
+        level = i + 1 
+        os.system('cls')
+        game(random_words[i], level)
+        print('Felicidades haz ganado!')
 
-def game(word):
+def game(word, level):
     attemps = 5
     founds = 0
     hidden_word = []
@@ -31,15 +35,15 @@ def game(word):
     for i in range(0, len(word)):
         hidden_word.append(' _')
 
+    print(f'\nLEVEL: {level}\n')
     for i in range(len(word)):
         print(' _', end='')
     
     while attemps > 0:
         if founds == len(word):
-            print('Felicidades haz ganado!')
             return
         else:
-            letter = input('\nEscribe una letra: ')
+            letter = input('\n\nEscribe una letra: ')
             #afirmacion: letter no puede ser un numero
             for i in range(0, len(word)):
                 if letter == hangmang[i][1]:
@@ -47,6 +51,7 @@ def game(word):
                     founds += 1
                     isHere = True
             os.system('cls')
+            print(f'\nLEVEL: {level}\n')
             print(''.join(hidden_word))
             if isHere != True:
                 attemps = attemps - 1
@@ -55,6 +60,7 @@ def game(word):
     
     if attemps == 0:
         print(f'Lo siento haz perdido la palabra era {word}')
+        exit()
            
 
 if __name__ == '__main__':
