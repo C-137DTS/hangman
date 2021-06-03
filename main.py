75f82c81e0b7cd977251cab056a9ffc14d6574c1
Juan Pablo <c137dts@gmail.com>
2021-06-02 20:06:11 -0500

    finishing the inputs and outputs

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+# 
+
 import random
 n_words = 171
 random_words = []
 random_lines = [random.randint(1, n_words) for i in range(1, 4)]
+
 
 
 def run():
@@ -11,6 +14,33 @@ def run():
             line += 1
             if line == random_lines[0] or line == random_lines[1] or line == random_lines[2]:
                 random_words.append(word.strip())
+    print(random_words[0])
+    game(random_words[0])
+
+def game(word):
+    attemps = 5
+    hidden_word = []
+    isHere = False
+    hangmang = list(enumerate(word))
+    for i in range(0, len(word)):
+        hidden_word.append(' _')
+
+    for i in range(len(word)):
+        print(' _', end='')
+    
+    while attemps > 0:
+        letter = input('\nEscribe una letra: ')
+        #afirmacion: letter no puede ser un numero
+        for i in range(0, len(word)):
+            if letter == hangmang[i][1]:
+                hidden_word[i] = hangmang[i][1].upper()
+                isHere = True
+        print(''.join(hidden_word))
+        if isHere != True:
+            attemps = attemps - 1
+        else:
+            isHere = False
+           
 
 if __name__ == '__main__':
     run()
